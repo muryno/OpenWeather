3ffba3b1d1ba622936b1f4f5abf28ba3108c586a
Muraino <muraino.yakubu@konga.com>
2021-11-01 17:49:10 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Open weather app
+
+
+
+### Architecture
+* Clean & MVVM
+
+### Library used
+* [LiveData](https://developer.android.com/topic/libraries/architecture/livedata) (Jetpack)
+* [ViewModel](https://developer.android.com/topic/libraries/architecture/viewmodel) (Jetpack)
+* [DataBinding](https://developer.android.com/topic/libraries/data-binding) (Jetpack)
+* [Room](https://developer.android.com/jetpack/androidx/releases/room) (Jetpack)
+* [Hilt](https://developer.android.com/training/dependency-injection/hilt-android)
+* [Retrofit2](https://square.github.io/retrofit/)
+* [Glide](https://github.com/bumptech/glide)
+* [Coroutine](https://developer.android.com/kotlin/coroutines?)
+* [UNIT TEST](https://developer.android.com/training/testing/unit-testing)
+* [ESPRESSO](https://github.com/bumptech/glide)
+* Using  https://open weather public  API
+
+
+### Features
+*  user, I can enter my location into the app .
+* swipe to refresh button
+* Mock server to test network
+* Espresso for unit testing
+* Clean architecture
+
+
+
+
---
+++ app/src/androidTest/java/com/muryno/openweather/MainActivityTest.kt
@@ -0,0 +1,57 @@
+package com.muryno.openweather
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.muryno.pokemonapp.utils.BaseUnitAndroidTest
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest{
+    @Rule
+    @JvmField
+    var mActivityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun displayScreenTitle() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.muryno.openweather", appContext.packageName)
+    }
+
+    @Test
+    fun validateEditText() {
+        onView(withId(R.id.location_input_field)).perform(ViewActions.typeText("location")).check(matches(withText("location")))
+    }
+
+    @Test
+    fun validateEditHintText() {
+        onView(withId(R.id.location_input_field))
+            .check(matches(withHint(R.string.enter_location)))
+    }
+
+
+
+    @Test
+    fun test_search_input_field() {
+        onView(withId(R.id.submit_button))
+            .check(matches(withText("Go")))
+            .perform(click())
+    }
+
+
+}
